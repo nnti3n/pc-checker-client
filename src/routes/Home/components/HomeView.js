1315e142ed94a901bb5482b8fd3792a7eb28d42f
Minh Nguyen <thienphong215@gmail.com>
2016-12-02 03:01:14 +0700

    Import Gakky

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import HomeImage from '../assets/Home.png'
+import Gakky from '../assets/Gakky.png'
 import classes from './HomeView.scss'
 
 export const HomeView = () => (
   <div>
     <h4>Welcome!</h4>
     <img
-      alt='hjx!'
+      alt='Gakky!'
       className={classes.duck}
-      src={HomeImage} />
-    <code>Khung search bấm ra ngoài hoặc bấm nút "Search" ms send request, làm z để khỏi stress server</code>
+      src={Gakky} />
+    <code>Second Deployment guys!</code>
   </div>
 )
 
